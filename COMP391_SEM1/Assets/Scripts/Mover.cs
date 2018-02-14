7922929218e7ba3fb9fae87cf16f279eb4ffa418
ShyamPanchal <shyampanchal95@gmail.com>
2018-02-14 14:18:30 -0500

    [add] laser and movement and restrictions

---
+++ COMP391_SEM1/Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    // Use this for initialization
+    private Rigidbody2D rb;
+    public float speed;
+	void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = this.transform.right * speed; // set velocity = (1,0)
+        
+	}
+    public void Update()
+    {
+        if (this.transform.position.x > 8.5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
--- COMP391_SEM1/Assets/Scripts/PlayerController.cs
+++ COMP391_SEM1/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
-	//used when performing physics calculations
-	void FixedUpdate(){
-		float moveHorizontal = Input.GetAxis ("Horizontal");//returns value between -1 and 1
-		float moveVertival = Input.GetAxis ("Vertical");//returns value between -1 and 1
-		/*if (!(moveVertival == 0 && moveHorizontal == 0)) {
-			Debug.Log ("H = " + moveHorizontal + " V = " + moveVertival);
-		}*/
-		Vector2 movement = new Vector2 (moveHorizontal, moveVertival);
-		Rigidbody2D rb = this.gameObject.GetComponent<Rigidbody2D> ();//establishes a connection to the rigid body 2d component
+    public float nextFire = 0.25f;
+    public float myTime = 0.0f;
+    public Boundary boundary;
+    Rigidbody2D rb;
+    public GameObject laser;
+    public Transform LaserSpawn;
+    private void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();   //establishes a connection to the rigid body 2d component
+        
+    }
+    //used when performing physics calculations
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis("Horizontal");//returns value between -1 and 1
+        float moveVertival = Input.GetAxis("Vertical");//returns value between -1 and 1
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertival);
 		rb.velocity = movement * speed;
-	}
+        rb.position = new Vector2(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 
+            Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax) );
+    }
+    private void Update()
+    {
+        myTime += Time.deltaTime;
+        if (Input.GetButton("Fire1") && myTime>nextFire)
+        {
+            Instantiate(laser, LaserSpawn.position, LaserSpawn.rotation);
+            myTime = 0.0f;
+        }
+    }
+}
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
 }
